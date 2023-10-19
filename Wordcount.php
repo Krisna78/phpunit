68b4a3cf01ac63995ea2494c45d800271cc5523f
E41220709_Yohanes Krisna Desantho <desanthokrisna78@gmail.com>
2023-10-19 13:38:03 +0700

    second update

--- Wordcount.php
+++ Wordcount.php
@@ -1,6 +1,9 @@
 <?php 
+// membuat class word count sebagai contoh library
 class Wordcount {
+    // dan membuat method untuk mengisi kata atau kalimat yang akan di masukkan ke parameter
     public function countWords($sentence) {
+        /* explode digunakan untuk memecah data yang memiliki space pada kalimat */
         return count(explode(" ",$sentence));
     }
 }
